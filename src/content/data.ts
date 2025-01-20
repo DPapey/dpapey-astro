a22c2a7fee294bcf398e4c1293a001e6a53542d7
DPapey <dpapey@outlook.com>
2025-01-20 18:09:53 +0000

    anchor for Blog

--- src/content/data.ts
+++ src/content/data.ts
@@ -19,25 +19,9 @@ export const skills = [
 
 export const projects = [
   {
-    title: "D&FINE",
+    title: "WIP",
     description:
-      "An e-commerce experience like no other. Shop fashion at D&FINE. Utilising Astro for...",
+      "An e-commerce experience like no other. Shop fashion at WIP. Utilising Astro for...",
   },
   {},
 ];
-
-/**
- * Icons adapted from https://phosphoricons.com/
- *
- * Want to add more?
- * 1. Find the icon you want on Phosphor Icons.
- * 2. Click “Copy SVG”.
- * 3. Paste the SVG code in your editor.
- * 4. Remove the `<svg>` wrapper so you only have elements like `<path>`, `<circle>`, `<rect>` etc.
- * 5. Remove any `stroke="#000000"` attributes
- * 6. Replace any `fill="#000000"` attributes with `stroke="none"`
- *    (or add `stroke="none"` on shapes with no `fill` or `stroke` specified).
- */
-export const iconPaths = {
-  "twitch-logo": `<path stroke-none d="M208,32H48A16,16,0,0,0,32,48V192a16,16,0,0,0,16,16H64v32a8,8,0,0,0,13.12,6.15L122.9,208h42.2a16,16,0,0,0,10.25-3.71l42.89-35.75A15.93,15.93,0,0,0,224,156.25V48A16,16,0,0,0,208,32Zm0,124.25L165.1,192H120a8,8,0,0,0-5.12,1.85L80,222.92V200a8,8,0,0,0-8-8H48V48H208ZM160,136V88a8,8,0,0,1,16,0v48a8,8,0,0,1-16,0Zm-48,0V88a8,8,0,0,1,16,0v48a8,8,0,0,1-16,0Z"/>`,
-};
